fd04cd60f754b85f2e399aeaf939f3738999b3d1
Afif Alfiano <m.afif@cakap.id>
2023-03-02 08:10:27 +0700

    BE: add swagger

--- apps/backend-rent/src/main.ts
+++ apps/backend-rent/src/main.ts
@@ -3,8 +3,10 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 import { AppModule } from './app/app.module';
 
@@ -12,11 +14,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  setupOpenApi(app);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+}
+
+function setupOpenApi(app: INestApplication) {
+  const config = new DocumentBuilder().setTitle('Rent Vehicles API Documentation').setVersion('1.0').addTag('api').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 
 bootstrap();
